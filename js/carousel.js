8c2ddb05f7857143996e9f534adf464ca0442abc
Mariana Macedo <marianamacedodefarias@gmail.com>
2025-12-22 21:36:14 -0300

    corações

--- js/carousel.js
+++ js/carousel.js
@@ -51,6 +51,9 @@ loveBtn.addEventListener('click', () => {
       countdown.classList.add('visible');
     }, 100);
   }, 600);
+
+  // Desbloquear o carrossel para permitir navegação novamente
+  document.querySelector('.carousel').classList.remove('locked');
 });
 
 // inicializa
